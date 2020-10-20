182c729bc995506472732bf28ac21dc06845ef75
Kansha <kansh1111@yahoo.com>
2020-10-20 18:01:08 +0800

    My portfolio site

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,24 @@
+import "../styles/styles.css";
+import img from '../images/imageOne.jpg';
+
+// Add the image to our existing div.
+
+
+
+// function component() {
+//   const element = document.querySelector('.page-section');
+//   element.classList.add('page-section__background')
+//   const myBgImg = new Image();
+//   myBgImg.src = img;
+//   element.appendChild(myBgImg);
+// }
+// component();
+  
+
+
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+
